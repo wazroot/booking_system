13212aff177b0f91518aaffdd4e96845cc77012c
akulavonen <aku.lavonen@gmail.com>
2020-11-30 14:24:12 +0200

    changed imports

--- resources/space.py
+++ resources/space.py
@@ -2,9 +2,10 @@
 from flask import request
 from flask_restful import Resource
 from http import HTTPStatus
-from models.instructions import Instruction
+from models.space import Space
+from models.reservation import Reservation
 from flask_jwt_extended import get_jwt_identity, jwt_required, jwt_optional
-from schemas.instruction import InstructionSchema
+from schemas.validations import InstructionSchema
 
 
 
