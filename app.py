507901c25ba4dbdee2ce6dfa037e2db368e63dc8
Juhamatti Vasamaa <juhamatti.vasamaa@edu.turkuamk.fi>
2020-11-30 11:15:31 +0200

    luotu def create_app, def register_extensions funktiot.

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
-import sqlalchemy
-import json
+
+from flask import Flask
+from flask_migrate import Migrate
+from flask_restful import Api
+from Config import Config
+from extensions import db, jwt
+from resources.user import UserListResource, UserResource, MeResource, UserInstructionListResource
+from resources.instruction import InstructionListResource, InstructionResource, InstructionPublic
+from resources.token import TokenResource, RefreshResource, RevokeResource, black_list
+# Muista muuttaa importit myöhemmin!!!
 
 
-print('Hello World') #testimoi
-print('toimiix')
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    app.app_context().push()
+    register_extensions(app)
+    register_resources(app)
+    return app
 
-print('toimii')
-print('toimiiko')
-
-print('Näyttäis toimivan ^__^')
-
-onnistuuko = False
-
-if not onnistuuko:
-    print("yritä")
-
-print('yes poks! wörkkii!')
-
-print('^_____________________________________________________^')
+def register_extensions(app):
+    db.app = app
+    db.init_app(app)
+    migrate = Migrate(app, db)
+    jwt.init_app(app)
