0e0e30a66e89e66447c8a8f1a54e0b1cfbf8bfea
Juhamatti Vasamaa <juhamatti.vasamaa@edu.turkuamk.fi>
2020-11-30 14:17:07 +0200

    Luotu luokka "SpaceResource"

--- resources/space.py
+++ resources/space.py
@@ -12,7 +12,7 @@ instruction_schema = InstructionSchema()
 instruction_list_schema = InstructionSchema(many=True)
 
 # Muista muuttaa instruction -> space
-class InstructionListResource(Resource):
+class SpaceListResource(Resource):
 
     def get(self):
 
@@ -35,4 +35,95 @@ class InstructionListResource(Resource):
 
         return instruction_schema.dump(instruction).data, HTTPStatus.CREATED
 
-        
+# Muista muuttaa instruction -> space
+class SpaceResource(Resource):
+
+    @jwt_optional
+    def get(self, instruction_id):
+
+        instruction = Instruction.get_by_id(instruction_id=instruction_id)
+
+
+        if instruction is None:
+            return {'message': 'instruction not found'}, HTTPStatus.NOT_FOUND
+
+        current_user = get_jwt_identity()
+
+        if instruction.is_publish == False and instruction.user_id != current_user:
+            return {'message': 'Access is not allowed'}, HTTPStatus.FORBIDDEN
+
+        return instruction_schema.dump(instruction).data, HTTPStatus.OK
+
+
+    @jwt_required
+    def put(self, instruction_id):
+
+        json_data = request.get_json()
+
+        instruction = Instruction.get_by_id(instruction_id=instruction_id)
+
+
+        if instruction is None:
+            return {'message': 'instruction not found'}, HTTPStatus.NOT_FOUND
+
+        current_user = get_jwt_identity()
+
+        if current_user != instruction.user_id:
+            return {'message': 'Access is not allowed'}, HTTPStatus.FORBIDDEN
+
+        instruction.name = json_data['name']
+        instruction.description = json_data['description']
+        instruction.steps = json_data['steps']
+        instruction.cost = json_data['cost']
+        instruction.tools = json_data['tools']
+        instruction.duration = json_data['duration']
+
+        instruction.save()
+
+        return instruction.data(), HTTPStatus.OK
+
+    @jwt_required
+    def delete(self, instruction_id):
+
+        instruction = Instruction.get_by_id(instruction_id=instruction_id)
+
+        if instruction is None:
+            return {'message': 'instruction not found'}, HTTPStatus.NOT_FOUND
+
+        current_user = get_jwt_identity()
+
+        if current_user != instruction.user_id:
+            return {'message': 'Access is not allowed'}, HTTPStatus.FORBIDDEN
+
+
+        instruction.delete()
+
+        return {}, HTTPStatus.NO_CONTENT
+
+    @jwt_required
+    def patch(self, instruction_id):
+        json_data = request.get_json()
+
+        data, errors = instruction_schema.load(data=json_data, partial=('name',))
+
+        if errors:
+            return {'message': 'Validation errors', 'errors': errors}, HTTPStatus.BAD_REQUEST
+
+        instruction = Instruction.get_by_id(instruction_id=instruction_id)
+
+        if instruction is None:
+            return {'message': 'Instruction not found'}, HTTPStatus.NOT_FOUND
+
+        current_user = get_jwt_identity()
+
+        if current_user != instruction.user_id:
+            return {'message': 'Access is not allowed'}, HTTPStatus.FORBIDDEN
+
+        instruction.name = data.get('name') or instruction.name
+        instruction.description = data.get('description') or instruction.description
+        instruction.steps = data.get('steps') or instruction.steps
+        instruction.tools = data.get('tools') or instruction.tools
+        instruction.cost = data.get('cost') or instruction.cost
+        instruction.duration = data.get('duration') or instruction.duration
+        instruction.save()
+        return instruction_schema.dump(instruction).data, HTTPStatus.OK
