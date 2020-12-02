ce2ef1ca09a67a9290d3c046997e09954d5b7daa
akulavonen <aku.lavonen@gmail.com>
2020-12-02 13:31:44 +0200

    Lisättiin rooli käyttäjälle ja testattiin käyttäjän luontia-> toimii!

--- schemas/user.py
+++ schemas/user.py
@@ -9,6 +9,7 @@ class UserSchema(Schema):
     username = fields.String(required=True)
     email = fields.Email(required=True)
     password = fields.Method(required=True, deserialize='load_password')
+    role = fields.Method(required=True)
     created_at = fields.DateTime(dump_only=True)
     updated_at = fields.DateTime(dump_only=True)
 
