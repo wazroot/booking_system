6ac564ea7cc728b7a4f16e4ab3cd93c1a12b307f
Juhamatti Vasamaa <juhamatti.vasamaa@edu.turkuamk.fi>
2020-12-02 11:33:22 +0200

    Added class ReservationPublic

--- resources/reservation.py
+++ resources/reservation.py
@@ -25,7 +25,7 @@ class ReservationListResource(Resource):
 class ReservationResource(Resource):
 
     @jwt_optional
-    def get(self, space_id):
+    def get(self, reservation_id):
 
         reservation = Reservation.get_by_id(reservation_id=reservation_id)
 
@@ -39,3 +39,104 @@ class ReservationResource(Resource):
 
         return reservation_schema.dump(reservation).data, HTTPStatus.OK
 
+    @jwt_required
+    def put(self, reservation_id):
+
+        json_data = request.get_json()
+
+        reservation = Reservation.get_by_id(reservation_id)
+
+
+        if reservation is None:
+            return {'message': 'reservation not found'}, HTTPStatus.NOT_FOUND
+
+        current_user = get_jwt_identity()
+
+        if current_user != reservation.user_id:
+            return {'message': 'Access is not allowed'}, HTTPStatus.FORBIDDEN
+
+
+        reservation.id = json_data['id']
+        reservation.time = json_data['time']
+
+        reservation.save()
+
+        return reservation.data(), HTTPStatus.OK
+
+    @jwt_required
+    def delete(self, reservation_id):
+
+        reservation = Reservation.get_by_id(reservation_id)
+
+        if reservation is None:
+            return {'message': 'reservation not found'}, HTTPStatus.NOT_FOUND
+
+        current_user = get_jwt_identity()
+
+        if current_user != reservation.user_id:
+            return {'message': 'Access is not allowed'}, HTTPStatus.FORBIDDEN
+
+
+        reservation.delete()
+
+        return {}, HTTPStatus.NO_CONTENT
+
+    @jwt_required
+    def patch(self, reservation_id):
+        json_data = request.get_json()
+
+        data, errors = reservation_schema.load(data=json_data, partial=('name',))
+
+        if errors:
+            return {'message': 'Validation errors', 'errors': errors}, HTTPStatus.BAD_REQUEST
+
+        reservation = Reservation.get_by_id(reservation_id=reservation_id)
+
+        if reservation is None:
+            return {'message': 'reservation not found'}, HTTPStatus.NOT_FOUND
+
+        current_user = get_jwt_identity()
+
+        if current_user != reservation.user_id:
+            return {'message': 'Access is not allowed'}, HTTPStatus.FORBIDDEN
+
+        reservation.time = data.get('time') or reservation.time
+        
+        reservation.save()
+        
+        return reservation_schema.dump(reservation).data, HTTPStatus.OK
+
+class ReservationPublic(Resource):
+    @jwt_required
+    def put(self, reservation_id):
+        reservation = Reservation.get_by_id(reservation_id=reservation_id)
+
+        if reservation is None:
+            return {'message': 'reservation not found'}, HTTPStatus.NOT_FOUND
+
+        current_user = get_jwt_identity()
+
+        if current_user != reservation.user_id:
+            return {'message': 'Access is not allowed'}, HTTPStatus.FORBIDDEN
+
+        reservation.is_publish = True
+        reservation.save()
+
+        return {}, HTTPStatus.NO_CONTENT
+
+    @jwt_required
+    def delete(self, reservation_id):
+        reservation = Reservation.get_by_id(reservation_id=reservation_id)
+
+        if reservation is None:
+            return {'message': 'reservation not found'}, HTTPStatus.NOT_FOUND
+
+        current_user = get_jwt_identity()
+
+        if current_user != reservation.user_id:
+            return {'message': 'Access is not allowed'}, HTTPStatus.FORBIDDEN
+
+        reservation.is_publish = False
+        reservation.save()
+
+        return {}, HTTPStatus.NO_CONTENT
