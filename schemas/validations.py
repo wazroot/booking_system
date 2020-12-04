5466cff06649dab547961a3405e8e25bd7df291d
akulavonen <aku.lavonen@gmail.com>
2020-12-04 10:02:44 +0200

    poistin kommentteja validationsista

--- schemas/validations.py
+++ schemas/validations.py
@@ -11,9 +11,8 @@ class SpaceSchema(Schema):
     id = fields.Integer(dump_only=True)
     name = fields.String(required=True, validate=[validate.Length(max=100)])
     capacity = fields.Integer(dump_only=True)
-    #is_publish = fields.Boolean(dump_only=True)
     created_at = fields.DateTime(dump_only=True)
-    #updated_at = fields.DateTime(dump_only=True)
+
 
     def validate_steps(n):
         if n < 1:
