cf5f8b448ef109764857bcec73568ee36070c0cc
akulavonen <aku.lavonen@gmail.com>
2020-12-04 15:08:16 +0200

    kommentoisin yhden käyttämättömän endpointin

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def register_resources(app):
     # space and user id.
     api.add_resource(ReservationSpaceTimeResource, '/reservations/<int:space_id>/<string:time>')  # to get reservations
     # with space_id and time
-    api.add_resource(UserReservationListResource, '/users/<string:username>/reservations')  # not yet implemented
+    #api.add_resource(UserReservationListResource, '/users/<string:username>/reservations')  # not yet implemented
 
 
 if __name__ == '__main__':
