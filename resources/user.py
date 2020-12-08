23afe518a7ee72fc6f2918da5ad75fe0aa4958ab
Juhamatti Vasamaa <74398913+wazroot@users.noreply.github.com>
2020-12-08 20:26:12 +0200

    couple additions

--- resources/user.py
+++ resources/user.py
@@ -1,5 +1,4 @@
 from flask import request, url_for
-from mailgun import MailgunApi
 from flask_restful import Resource
 from http import HTTPStatus
 from utils import hash_password, generate_token, verify_token
@@ -9,32 +8,35 @@ from schemas.user import UserSchema
 from http import HTTPStatus
 from webargs import fields
 from webargs.flaskparser import use_kwargs
-from models.space import Space  # Instructions muuttui -> Space
+from models.space import Space                      #Instructions muuttui -> Space
 from schemas.validations import SpaceSchema
+from schemas.user import UserSchema
+from mailgun import MailgunApi
 
-# instructions_list_schema muuttui -> space_list_schema
-space_list_schema = SpaceSchema(many=True)  # InstructionSchema muuttui -> SpaceSchema
+                                                   #instructions_list_schema muuttui -> space_list_schema
+space_list_schema = SpaceSchema(many=True)         #InstructionSchema muuttui -> SpaceSchema
 user_schema = UserSchema()
-user_public_schema = UserSchema(exclude=('email',))
+user_public_schema = UserSchema(exclude=('email', ))
 mailgun = MailgunApi(domain='sandboxe6b1c63a9c6c44a39c6525059bd73550.mailgun.org',
- api_key='eee238a177c8cf694348fec9b10efbfd-95f6ca46-63e18c82')
+                     api_key='eee238a177c8cf694348fec9b10efbfd-95f6ca46-63e18c82')
+
+class UserSpaceListResource(Resource):
+    @jwt_optional
+    @use_kwargs({'visibility': fields.Str(missing='public')})
+    def get(self, username, visibility):
+        user = User.get_by_username(username=username)
+        if user is None:
+            return {'message': 'User not found'}, HTTPStatus.NOT_FOUND
+        current_user = get_jwt_identity()
+        if current_user == user.id and visibility in ['all', 'private']:
+            pass
+        else:
+            visibility = 'public'
+        space = Space.get_all_by_user(user_id=user.id,visibility=visibility)
+        return space_list_schema.dump(space).data, HTTPStatus.OK
 
 
-
-# class UserSpaceListResource(Resource):
-#     @jwt_optional
-#     @use_kwargs({'visibility': fields.Str(missing='public')})
-#     def get(self, username, visibility):
-#         user = User.get_by_username(username=username)
-#         if user is None:
-#             return {'message': 'User not found'}, HTTPStatus.NOT_FOUND
-#         current_user = get_jwt_identity()
-#         if current_user == user.id and visibility in ['all', 'private']:
-#             pass
-#         else:
-#             visibility = 'public'
-#         space = Space.get_all_by_user(user_id=user.id,visibility=visibility)
-#         return space_list_schema.dump(space).data, HTTPStatus.OK
+#class UserReservationListResource(Resource):
 
 
 class MeResource(Resource):
@@ -42,7 +44,6 @@ class MeResource(Resource):
     def get(self):
         user = User.get_by_id(id=get_jwt_identity())
         return user_schema.dump(user).data, HTTPStatus.OK
-
 
 class UserResource(Resource):
     @jwt_optional
@@ -57,7 +58,6 @@ class UserResource(Resource):
             data = user_public_schema.dump(user).data
         return data, HTTPStatus.OK
 
-
 class UserListResource(Resource):
     def post(self):
         json_data = request.get_json()
@@ -70,26 +70,4 @@ class UserListResource(Resource):
             return {'message': 'email already used'}, HTTPStatus.BAD_REQUEST
         user = User(**data)
         user.save()
-        token = generate_token(user.email, salt='activate')
-        subject = 'Please confirm your registration.'
-        link = url_for('useractivateresource', token=token, _external=True)
-        text = 'Hi, Thanks for using our Booking system! Please confirm your registration by clicking on the link: {}'.format(link)
-        mailgun.send_email(to=user.email, subject=subject, text=text)
         return user_schema.dump(user).data, HTTPStatus.CREATED
-
-
-class UserActivateResource(Resource):
-    def get(self, token):
-        email = verify_token(token, salt='activate')
-        if email is False:
-            return {'message': 'Invalid token or token expired'}, HTTPStatus.BAD_REQUEST
-
-        user = User.get_by_email(email=email)
-        if not user:
-            return {'message': 'User not found'}, HTTPStatus.NOT_FOUND
-        if user.is_active is True:
-            return {'message': 'The user account is already activated'}, HTTPStatus.BAD_REQUEST
-        
-        user.is_active = True
-        user.save()
-        return {}, HTTPStatus.NO_CONTENT
