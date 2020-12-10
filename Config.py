0ed2ce1f3df3a15d5ce29a60bcbc272f56e7d526
Juhamatti Vasamaa <74398913+wazroot@users.noreply.github.com>
2020-12-10 18:18:43 +0200

    sekoilua

--- Config.py
+++ Config.py
@@ -14,15 +14,15 @@ class DevelopmentConfig(Config):
     
     DEBUG = True
     SECRET_KEY = 'super-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:12345@localhost/database'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wazroot:19Piippolanvaari88@localhost/projekti'
 
 
 class ProductionConfig(Config):
     
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:12345@localhost/database'
+    #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:12345@localhost/database'
     #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class StagingConfig(Config):
     SECRET_KEY = os.environ.get('SECRET_KEY')
