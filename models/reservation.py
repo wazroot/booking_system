c11354f804d7751eaca18b7ec8093b254bcf4e6c
Ville Rytinki <ville.rytinki@edu.turkuamk.fi>
2020-12-11 12:45:45 +0200

    Reservation class updated.

--- models/reservation.py
+++ models/reservation.py
@@ -20,7 +20,7 @@ class Reservation(db.Model):
     user_id = db.Column(db.Integer(), nullable=False)
     space_id = db.Column(db.Integer(), nullable=False)
     created_at = db.Column(db.DateTime(), nullable=False, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime(), nullable=False, server_default=db.func.now(), onupdate=db.func.now())
+    #updated_at = db.Column(db.DateTime(), nullable=False, server_default=db.func.now(), onupdate=db.func.now())
 
 
     @classmethod
