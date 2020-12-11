5e2976a457b5bcfd3f87f445fda463580cb23808
Ville Rytinki <ville.rytinki@edu.turkuamk.fi>
2020-12-11 13:14:18 +0200

    test method in validations.py validate_time for validating time only.

--- schemas/validations.py
+++ schemas/validations.py
@@ -1,5 +1,6 @@
 from marshmallow import Schema, fields, post_dump, validate, validates, ValidationError
 from schemas.user import UserSchema
+from models.reservation import Reservation
 
 
 class SpaceSchema(Schema):
@@ -46,14 +47,18 @@ class SpaceSchema(Schema):
 
 
 class ReservationSchema(Schema):
+
+    def validate_time(self, time_validated):
+        all_reservations = Reservation.get_all_reservations()
+        for i in range(0, (list(all_reservations).count(all_reservations) - 1)):
+            if all_reservations[i]["time"] == time_validated:
+                raise ValidationError('already taken')
+
     id = fields.Integer(dump_only=True)
-    time = fields.Date(required=True)
+    time = fields.Date(required=True, validate=validate_time)
     user_id = fields.Integer(required=True)
     space_id = fields.Integer(required=True)
     created_at = fields.DateTime(dump_only=True)
     updated_at = fields.DateTime(dump_only=True)
 
-    #@validates('time')
-    #def validate_time(self,f):
-    #    if f == time:
-    #        raise ValidationError('already taken')
+
