2a98d7d671b718030250b8e27927b58d05a9a26c
Ville Rytinki <ville.rytinki@edu.turkuamk.fi>
2020-12-11 15:12:36 +0200

    returned to previous state.

--- resources/reservation.py
+++ resources/reservation.py
@@ -29,7 +29,7 @@ class ReservationListResource(Resource):
         data, errors = reservation_schema.load(data=json_data)
 
         # get all reservations and make it a list
-        existing_reservations = Reservation.query.filter_by(time=json_data['time'], space_id=json_data['space_id']).all()
+        existing_reservations = Reservation.query.filter_by(time=json_data['time'], space_id=json_data['space_id'])
         if len(existing_reservations) > 0:
             return {'message': "A reservation already exists for given time and space"}, HTTPStatus.BAD_REQUEST
 
